b45a5bd46ed79f8b975dda8f59a11fc568e1ffb7
EthanWelsh <welshej@gmail.com>
2015-03-21 18:16:13 -0400

    complete genetic algorithm

--- GeneticSudoku.go
+++ GeneticSudoku.go
@@ -27,19 +27,27 @@ func main() {
 		population[i] = getRandomGene(startBoard)
 	}
 
-	fmt.Println(getPopulationStats(population))
-	population = evolve(population, 100)
-	fmt.Println(getPopulationStats(population))
+	for i := 0; i < 10; i++ {
+		avg, max, min := getPopulationStats(population)
+		fmt.Printf("%d).\t\t\tAVG: %.2f\t\tMAX: %d\t\tMIN: %d\n", i, avg, max, min)
+		population = evolve(population, 10, .001)
+	}
 
 }
 
 func evolve(population []Gene, iterations int, chanceAtMutation float64) []Gene {
 
+	mutationsMade := 0
+
 	for i := 0; i < iterations; i++ {
 		population = getNextGeneration(population)
+
+		for i := range population {
+			population[i].Mutate(chanceAtMutation)
+		}
 	}
 
-	// TODO Add mutation
+	fmt.Println("--", mutationsMade, "--")
 
 	return population
 }
@@ -56,13 +64,13 @@ func getNextGeneration(oldPopulation []Gene) (newPopulation []Gene) {
 		phenotypeA := randomGeneSelector.Spin()
 		phenotypeB := randomGeneSelector.Spin()
 
-		newPopulation[i] = mateGenes(phenotypeA, phenotypeB)
+		newPopulation[i] = mateGenes(&phenotypeA, &phenotypeB)
 	}
 
 	return
 }
 
-func getPopulationStats(population []Gene) (avg uint64, max uint64, min uint64) {
+func getPopulationStats(population []Gene) (avg float64, max uint64, min uint64) {
 
 	var total uint64 = 0
 	var geneScore uint64 = 0
@@ -85,14 +93,14 @@ func getPopulationStats(population []Gene) (avg uint64, max uint64, min uint64) 
 		}
 	}
 
-	avg = total / uint64(len(population))
+	avg = float64(total) / float64(len(population))
 
 	return
 
 }
 
 // Given a specific gene, will get the board for that gene
-func getBoardFromGene(gene Gene) Board {
+func getBoardFromGene(gene *Gene) Board {
 
 	board := Init()
 	var index int
@@ -110,56 +118,4 @@ func getBoardFromGene(gene Gene) Board {
 // Generates a random number between min and max (inclusive)
 func random(min int, max int) int {
 	return rand.Intn(max) + min
-}
-
-// Given a bit string, will provide the number which maps to that bit string
-func bitStringToNum(s string) int {
-	switch s {
-	case "0001":
-		return 1
-	case "0010":
-		return 2
-	case "0011":
-		return 3
-	case "0100":
-		return 4
-	case "0101":
-		return 5
-	case "0110":
-		return 6
-	case "0111":
-		return 7
-	case "1000":
-		return 8
-	case "1001":
-		return 9
-	default:
-		return 0
-	}
-}
-
-// Given a num, will give the bit string that corresponds to that number
-func numToBitString(num int) string {
-	switch num {
-	case 1:
-		return "0001"
-	case 2:
-		return "0010"
-	case 3:
-		return "0011"
-	case 4:
-		return "0100"
-	case 5:
-		return "0101"
-	case 6:
-		return "0110"
-	case 7:
-		return "0111"
-	case 8:
-		return "1000"
-	case 9:
-		return "1001"
-	default:
-		return "0000"
-	}
 }
--- gene.go
+++ gene.go
@@ -1,56 +1,149 @@
 package main
 
+import (
+//"fmt"
+)
+
 const GENE_SIZE = NUMBER_OF_ROWS * NUMBER_OF_COLS
+const PROB_GRANULARITY = 100000
+const MATE_CHANCES = 100
 
 type Gene struct {
 	gene [GENE_SIZE]string
 }
 
 func (g *Gene) Score() int {
-	b := getBoardFromGene(*g)
+	b := getBoardFromGene(g)
 	return b.Grade()
 }
 
-func mateGenes(a Gene, b Gene) (res Gene) {
+func (g *Gene) Mutate(chanceAtMutation float64) (mutationsMade int) {
 
-	r := random(1, GENE_SIZE-1)
-	aFirst := random(0, 1)
+	if chanceAtMutation == 0 {
+		return 0
+	}
 
-	for i := 0; i < r; i++ {
-		if aFirst == 1 {
+	for i, _ := range g.gene {
+		chanceAtMutation = chanceAtMutation * PROB_GRANULARITY
+		r := random(0, PROB_GRANULARITY)
+
+		if float64(r) < chanceAtMutation { // time to mutate!
+
+			temp := *g
+			rand := random(1, 9+NUMBER_OF_CHANCES_FOR_UNASSIGNED)
+			g.gene[i] = numToBitString(rand)
+
+			if !getBoardFromGene(g).PossibleBoard() {
+				g = &temp
+			} else {
+				mutationsMade++
+			}
+
+		}
+	}
+
+	return
+}
+
+func mateGenes(a *Gene, b *Gene) (res Gene) {
+
+	firstIteration := true
+
+	for i := 0; firstIteration || !getBoardFromGene(&res).PossibleBoard(); i++ {
+
+		//To prevent deadlock, after a certain amount of unsuccessful mating attempts, just return the high board
+		if i >= MATE_CHANCES {
+			if a.Score() > b.Score() {
+				return *a
+			} else {
+				return *b
+			}
+		}
+
+		firstIteration = false
+		r := random(1, GENE_SIZE)
+
+		for i := 0; i < r; i++ {
 			res.gene[i] = a.gene[i]
-		} else {
+		}
+		for i := r; i < GENE_SIZE; i++ {
 			res.gene[i] = b.gene[i]
 		}
 	}
-	for i := r; i < GENE_SIZE; i++ {
-		if aFirst == 1 {
-			res.gene[i] = b.gene[i]
-		} else {
-			res.gene[i] = a.gene[i]
-		}
-	}
+
 	return
 }
 
 // Generates a random gene sequence that represents a possible partial solution to the given board
-// TODO Non-Recursive Solution
 func getRandomGene(b Board) (g Gene) {
 
-	for r := 0; r < NUMBER_OF_ROWS; r++ {
-		for c := 0; c < NUMBER_OF_COLS; c++ {
-			if b.Get(r, c) == UNASSIGNED {
-				rand := random(1, 9+NUMBER_OF_CHANCES_FOR_UNASSIGNED)
-				g.gene[c+(r*9)] = numToBitString(rand)
-			} else {
-				g.gene[c+(r*9)] = numToBitString(b.Get(r, c))
+	firstIteration := true
+
+	for firstIteration || !getBoardFromGene(&g).PossibleBoard() {
+		firstIteration = false
+		for r := 0; r < NUMBER_OF_ROWS; r++ {
+			for c := 0; c < NUMBER_OF_COLS; c++ {
+				if b.Get(r, c) == UNASSIGNED {
+					rand := random(1, 9+NUMBER_OF_CHANCES_FOR_UNASSIGNED)
+					g.gene[c+(r*9)] = numToBitString(rand)
+				} else {
+					g.gene[c+(r*9)] = numToBitString(b.Get(r, c))
+				}
 			}
 		}
 	}
 
-	if getBoardFromGene(g).PossibleBoard() {
-		return g
-	} else {
-		return getRandomGene(b)
+	return g
+}
+
+// Given a bit string, will provide the number which maps to that bit string
+func bitStringToNum(s string) int {
+	switch s {
+	case "0001":
+		return 1
+	case "0010":
+		return 2
+	case "0011":
+		return 3
+	case "0100":
+		return 4
+	case "0101":
+		return 5
+	case "0110":
+		return 6
+	case "0111":
+		return 7
+	case "1000":
+		return 8
+	case "1001":
+		return 9
+	default:
+		return 0
+	}
+}
+
+// Given a num, will give the bit string that corresponds to that number
+func numToBitString(num int) string {
+	switch num {
+	case 1:
+		return "0001"
+	case 2:
+		return "0010"
+	case 3:
+		return "0011"
+	case 4:
+		return "0100"
+	case 5:
+		return "0101"
+	case 6:
+		return "0110"
+	case 7:
+		return "0111"
+	case 8:
+		return "1000"
+	case 9:
+		return "1001"
+	default:
+		return "0000"
 	}
 }
